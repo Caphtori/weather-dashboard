577543c90e726b1442bfe8963f0cdafe9f66d27e
Qohelet <benjy.shuter@gmail.com>
2023-08-28 23:36:46 -0400

    Adds the header and the beginning elements of the rest of the site.

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// let fetchUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6f70b9885c5a3d8ea9b7077692618ee1}";
+
+// let fetchUrl = "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={6f70b9885c5a3d8ea9b7077692618ee1}"
+// fetch(fetchUrl)
+//     .then((response)=>{
+//         return response.json();
+//     })
+//     .then((data)=>{
+//         console.log(data);
+//     })
+
+
